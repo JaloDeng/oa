f56dbbb15e4142757853d090def3a6df67393391
Jalo <651379728@qq.com>
2017-12-31 22:35:20 +0800

    Add User view.

--- src/main/java/com/jalo/mis/oa/controller/UserController.java
+++ src/main/java/com/jalo/mis/oa/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.jalo.mis.oa.controller;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.jalo.mis.oa.entity.UserEntity;
 import com.jalo.mis.oa.model.User;
@@ -21,8 +24,7 @@ import com.jalo.mis.oa.service.UserService;
 
 /**
  * Controller : user
- * 
- * @author Jalo Asus
+ * @author Jalo
  */
 @RestController
 @RequestMapping("/user")
@@ -31,25 +33,65 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@GetMapping("")
-	public @ResponseBody List<UserEntity> findAll(HttpServletRequest request, HttpServletResponse response) {
+	@GetMapping("/all")
+	public @ResponseBody List<UserEntity> userAll() {
 		return userService.findAll();
 	}
-
+	
+	@GetMapping
+	public ModelAndView findAll(Model model, HttpServletRequest request, HttpServletResponse response) {
+		model.addAttribute("list", userService.findAll());
+		model.addAttribute("title", "用户列表");
+		return new ModelAndView("oa/list", "result", model);
+	}
+	
 	@GetMapping("/{id}")
-	public @ResponseBody UserEntity find(@PathVariable Long id, HttpServletRequest request,
+	public ModelAndView find(@PathVariable Long id, Model model, HttpServletRequest request,
 			HttpServletResponse response) {
-		return userService.findOne(id);
+		model.addAttribute("user", userService.findOne(id));
+		model.addAttribute("title", "查看用户");
+		return new ModelAndView("oa/view", "result", model);
 	}
 
-	@PostMapping("")
-	public @ResponseBody UserEntity saveOrUpdate(@RequestBody User model, HttpServletRequest request,
-			HttpServletResponse response) {
-		return userService.saveOrUpdate(model);
+	@GetMapping("/form")
+	public ModelAndView form(Model model, HttpServletRequest request, HttpServletResponse response) {
+		model.addAttribute("user", new User());
+		model.addAttribute("title", "创建用户");
+		return new ModelAndView("oa/form", "result", model);
+	}
+	
+	@GetMapping("/update/{id}")
+	public ModelAndView form(@PathVariable Long id, Model model,  HttpServletRequest request, HttpServletResponse response) {
+		model.addAttribute("user", userService.findOne(id));
+		model.addAttribute("title", "修改用户");
+		return new ModelAndView("oa/form", "result", model);
+	}
+	
+	@PostMapping
+	public ModelAndView saveOrUpdate(@RequestBody User user, HttpServletRequest request, HttpServletResponse response) {
+		userService.saveOrUpdate(user);
+		return new ModelAndView("redirect:/user");
+	}
+	
+	@PostMapping("/form")
+	public ModelAndView saveOrUpdateForm(User user, HttpServletRequest request, HttpServletResponse response) {
+		userService.saveOrUpdate(user);
+		return new ModelAndView("redirect:/user");
 	}
 	
 	@DeleteMapping("/{id}")
-	public @ResponseBody Long delete(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response) {
-		return userService.delete(id);
+	public void delete(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response) {
+		userService.delete(id);
+		try {
+			response.sendRedirect("/user");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@GetMapping("delete/{id}")
+	public ModelAndView deleteByMethodGet(@PathVariable Long id, HttpServletRequest request, HttpServletResponse response) {
+		userService.delete(id);
+		return new ModelAndView("redirect:/user");
 	}
 }
